fc05be58f2c369c940b363c83319093dc4bf627e
Dhruv Jain <dhruvkc16@gmail.com>
2017-01-22 18:13:14 -0500

    Create reverse_string.cpp

---
+++ cpp_solutions/reverse_string.cpp
@@ -0,0 +1,14 @@
+//Write a function that takes a string as input and returns the string reversed.
+
+class Solution {
+public:
+    string reverseString(string s) {
+        int i=0, j=s.size() - 1;
+        while(i<j){
+            swap(s[i++],s[j--]);
+        }
+        return s;
+        
+        
+    }
+};
