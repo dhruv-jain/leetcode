8ebde64e0422b0874d606275c75ef243c3d9f64a
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-15 22:20:07 -0400

    Create palindrom_linked_list.cpp

---
+++ cpp_solutions/palindrom_linked_list.cpp
@@ -0,0 +1,10 @@
+https://www.youtube.com/watch?v=oZuR2-AKkXE 
+
+a b c d e f g 
+
+1. Split the list into two parts from the middle. 
+2. Prepare the two linked lists 
+3. If the list is odd then ignore the middle. 
+4. Reverse the second linked list. 
+5. Compare the two linked lists. 
+
