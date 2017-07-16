b6076ee913d35b76aedf9e3bc62a2eba4a37a149
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-15 22:59:03 -0400

    Update palindrom_linked_list.cpp

--- cpp_solutions/palindrom_linked_list.cpp
+++ cpp_solutions/palindrom_linked_list.cpp
@@ -1,4 +1,4 @@
-https://www.youtube.com/watch?v=oZuR2-AKkXE 
+/*https://www.youtube.com/watch?v=oZuR2-AKkXE 
 
 a b c d e f g 
 
@@ -7,4 +7,21 @@ a b c d e f g
 3. If the list is odd then ignore the middle. 
 4. Reverse the second linked list. 
 5. Compare the two linked lists. 
+*/
 
+ ListNode* p;
+        ListNode* q;
+        ListNode* start_second;
+        while(1) {
+            p = p->next->next;
+            if(p->next==NULL) {
+                start_second = q->next->next;
+                break;
+            }
+            if(p==NULL) {
+                start_second = q->next; 
+                break;
+            }
+            q=q->next;
+        }
+        q->next=NULL;
