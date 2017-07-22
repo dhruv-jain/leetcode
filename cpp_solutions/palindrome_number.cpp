1f7430f3895be81f510f791303676cc2c504d12a
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-22 14:25:54 -0400

    Update palindrome_number.cpp

--- cpp_solutions/palindrome_number.cpp
+++ cpp_solutions/palindrome_number.cpp
@@ -5,24 +5,19 @@ Could negative integers be palindromes? (ie, -1)
 
 If you are thinking of converting the integer to string, note the restriction of using extra space.
 
-You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", you know that the reversed integer might overflow. How would you handle such case?
+You could also try reversing an integer. However, if you have solved the problem "Reverse Integer", 
+you know that the reversed integer might overflow. How would you handle such case?
 
 There is a more generic way of solving this problem.*/
 
-
-
-
 class Solution {
 public:
-    bool isPalindrome(int x) {
-        
+    bool isPalindrome(int x) { 
         if (x<0 || (x!=0 && x%10==0)){
             return false;
         }
-        
         int sum =0;
-        while(x>sum){
-            
+        while(x>sum){   
             sum = sum*10 + x%10;
             x=x/10;
         }
