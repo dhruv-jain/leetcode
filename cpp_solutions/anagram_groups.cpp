82abf10bfd8b1ef70d7c4723e906a1d968ea1800
Dhruv Jain <dhruvkc16@gmail.com>
2017-07-23 13:12:13 -0400

    Create anagram_groups.cpp

---
+++ cpp_solutions/anagram_groups.cpp
@@ -0,0 +1,22 @@
+//https://leetcode.com/problems/group-anagrams/#/description
+
+class Solution {
+public:
+    vector<vector<string>> groupAnagrams(vector<string>& strs) {
+        
+        unordered_map<string, vector<string>> umap;
+        
+        for(int i =0;i<strs.size();i++){
+            string temp = strs[i];
+            sort(temp.begin(), temp.end());
+            umap[temp].push_back(strs[i]);      //sorted keys 
+        }
+        
+        std::vector<std::vector<std::string>> result;
+        
+        for(unordered_map<string, vector<string>>::iterator it = umap.begin(); it != umap.end(); ++it) {
+            result.push_back(it->second);
+        }
+        return result;        
+    }
+};
