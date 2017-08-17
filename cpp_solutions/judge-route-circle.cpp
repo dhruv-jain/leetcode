6146863265b6a3bb4a3ab56d2cfeb90541c1d404
Dhruv Jain <dhruvkc16@gmail.com>
2017-08-17 16:02:31 -0400

    Update judge-route-circle.cpp

--- cpp_solutions/judge-route-circle.cpp
+++ cpp_solutions/judge-route-circle.cpp
@@ -13,3 +13,21 @@ Example 2:
 Input: "LL"
 Output: false
 */
+
+class Solution {
+public:
+    bool judgeCircle(string moves) {
+        int v = 0;
+        int h = 0;
+        
+        for (char ch : moves) {
+            switch(ch) {
+                case 'U': v++; break;
+                case 'D': v--; break;
+                case 'L': h--; break;
+                case 'R': h++; break;
+            }
+        }
+         return v==0 && h==0;
+    }
+};
