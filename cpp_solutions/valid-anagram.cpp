cbe26d2f2221ba1b9e33ebf18a23a3e0b2bb2313
Dhruv Jain <dhruvkc16@gmail.com>
2017-08-17 22:45:20 -0400

    Update valid-anagram.cpp

--- cpp_solutions/valid-anagram.cpp
+++ cpp_solutions/valid-anagram.cpp
@@ -25,3 +25,22 @@ public:
         
     }
 };
+
+//METHOD 2
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.length()!=t.length()) { 
+            return false;
+        }
+        unordered_map<char, int> counts;
+        for(int i=0;i<s.length();i++){
+            counts[s[i]]++;
+            counts[t[i]]--;
+        }
+        for(auto count : counts) 
+            if(count.second) return false;
+        return true; 
+    }
+};
