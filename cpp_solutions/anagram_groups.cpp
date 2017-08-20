cb1fd2bc5ee25672c5d0fb824b272a5cfa93f0fe
Dhruv Jain <dhruvkc16@gmail.com>
2017-08-20 14:32:27 -0400

    Update anagram_groups.cpp

--- cpp_solutions/anagram_groups.cpp
+++ cpp_solutions/anagram_groups.cpp
@@ -1,4 +1,14 @@
 //https://leetcode.com/problems/group-anagrams/#/description
+/*Given an array of strings, group anagrams together.
+
+For example, given: ["eat", "tea", "tan", "ate", "nat", "bat"], 
+Return:
+
+[
+  ["ate", "eat","tea"],
+  ["nat","tan"],
+  ["bat"]
+]*/
 
 class Solution {
 public:
