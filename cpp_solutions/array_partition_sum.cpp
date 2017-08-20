cebb6453a312b99be60335df48c63dd996f9ffe4
Dhruv Jain <dhruvkc16@gmail.com>
2017-08-20 14:37:52 -0400

    Update array_partition_sum.cpp

--- cpp_solutions/array_partition_sum.cpp
+++ cpp_solutions/array_partition_sum.cpp
@@ -9,7 +9,6 @@ Explanation: n is 2, and the maximum sum of pairs is 4 = min(1, 2) + min(3, 4).
 class Solution {
 public:
     int arrayPairSum(vector<int>& nums) {
-        
         int sum=0;
         sort(nums.begin(), nums.end());
         for(int i=0;i<nums.size();i+=2){
