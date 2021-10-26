d05fdd1cfc3968cb49df5bbca4c7751df44dc38d
Dhruv Jain <djain@clemson.edu>
2021-10-26 13:58:33 -0400

    Create lengthOfLongestSubstring.py

---
+++ python/lengthOfLongestSubstring.py
@@ -0,0 +1,22 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        
+        #brute force will be o(n3) find all substrings 
+        #find repeating char and keep adding 1
+        #use dictionary to find if you come across same char 
+        
+        if len(s) ==0:
+            return 0 
+        
+        map = {}
+        max_length = start = 0 
+        
+        for i in range(len(s)):
+            if s[i] in map and start <= map[s[i]]:
+                start = map[s[i]] + 1 
+            else:
+                max_length = max(max_length, i -start+1)
+            
+            map[s[i]] = i 
+        return (max_length)
+        
